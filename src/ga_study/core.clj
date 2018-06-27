53cc997ae726b4db4b685d538b32f06e297ada0f
Akira Ota 10nin <akira19870221@gmail.com>
2018-06-27 20:36:03 +0900

    Add calclate x value and target function.

--- src/ga_study/core.clj
+++ src/ga_study/core.clj
@@ -5,3 +5,9 @@
   (if (= (count b) 0) 0
       (+ (if (= 1 (first b)) (Math/expt 2 (- (count b) 1))
              0) (bin->dec (rest b)))))
+(defn get_x [b]
+  (/ (bin->dec b) (- (Math/expt 2 (count b)) 1)))
+  
+(defn target_f [x]
+  (+ (java.lang.Math/sin (* 3 x)) (* 0.5 (java.lang.Math/sin (* 9 x))) (java.lang.Math/sin (+ (* 15 x) 50))))
+  
